ea12f2ccf641a030f6f7792a84275755ca8b1779
Ruan Petrus Alves Leite <xastroboyx11@gmail.com>
2022-12-21 15:39:07 -0300

    Add template file

---
+++ template.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+using namespace std;
+//g++ -std=c++17 -O2 -Wall run.cpp -o run
+
+#define int long long
+#define endl "\n"
+#define sws std::ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define all(x) x.begin(), x.end()
+#define dbg(msg, x) cout << msg << " = " << x << endl
+#define pb push_back
+#define mp make_pair
+#define TETO(a, b) ((a) + (b-1))/(b)
+typedef vector<int> vi;
+typedef pair<int,int> pii;
+const long long MOD = 1e9+7;
+const long long LLINF = 0x3f3f3f3f3f3f3f3f;
+
+void solve(){
+
+}
+
+int32_t main(){ sws; 
+    int t = 1;
+    // cin >> t;
+    for (int i = 0; i < t; i++){
+        solve();
+    }
+
+    return 0;
+}
